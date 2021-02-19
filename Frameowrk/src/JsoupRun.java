83dba1da0caed1fb10377f29698011e6343d247e
DRAZIW64 <56693500+DRAZIW64@users.noreply.github.com>
2021-02-19 07:59:01 +0100

    Edit and progress
    Edit and progress

--- Frameowrk/src/JsoupRun.java
+++ Frameowrk/src/JsoupRun.java
@@ -10,16 +10,22 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.swing.text.View;
+
 public class JsoupRun {
 
-	public static void main(String[] args) throws IOException {
+	
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
 		
 		//URL ENTRY
-		Document doc = Jsoup.connect("https://euw.op.gg/summoner/userName=kofolac")
+		Document doc = Jsoup.connect("https://euw.op.gg/summoner/userName=ZPG+Thornight")
 				.timeout(6000).get();
 		//System.out.println(doc);
 		ArrayList<String> ar = new ArrayList<String>();
-		//int count = 0;
+		ArrayList<String> wr = new ArrayList<String>();
+		ArrayList<String> ant = new ArrayList<String>();
 		
 		
 		//SCRAPING SELECTION // 
@@ -27,97 +33,73 @@ public class JsoupRun {
 		//System.out.println(name.text());
 		String savefile = name.text();
 		//System.out.println(savefile);
-		
-		Elements profpic = doc.select("div.ProfileIcon");
+
+		//Elements profpic = doc.select("div.ProfileIcon");
 		//System.out.println(profpic.select("img"));
 		
 		Elements maintier = doc.select("div.Tierbox.Box");
+		Elements mtier = maintier.select("div.TierRank");
 		Elements subtier = doc.select("div.sub-tier");
+		Elements subt = subtier.select("div.sub-tier__rank-tier");
+		//System.out.println(subt.text());
 		//System.out.println(maintier.select("img"));
 		
 		Elements wratio = doc.select("div.WinRatioTitle");
 		//System.out.println(wratio);	
 		
+		
 		Elements champions = doc.select("div.MostChampionContent");
 		for (Element e : champions.select("div.ChampionBox.Ranked")) {
-			int number;
 			//System.out.println(e.text());
 			//System.out.println(e.select("a").text());
+			String percent = e.select("div.WinRatio").text();
 			String played = e.select("a").text();
+			String ant1 = e.select("div.Title").text();
 			System.out.println(played);
 			ar.add(played);
-			Document docTwo = Jsoup.connect("https://u.gg/lol/champions/"+played+"/build")
-					.timeout(6000).get();
-			Elements Uname = docTwo.select("div.champion-header-info");
-			
-			System.out.println("Test" + Uname.select("span.champion-name").text());
-			
-			
-			//count = (count+1);
-			//System.out.println(count);
+			wr.add(percent);
+			ant.add(ant1);
+						//System.out.println("Test" + Uname.select("span.champion-name").text());
 		}
 		
-		//for(int k = 0; k < 5; k++) { 
-		//	System.out.println(ar);
-		//Document docTwo = Jsoup.connect("https://u.gg/lol/champions/"+ar+"/build")
-		//		.timeout(6000).get();
-		//Elements Uname = docTwo.select("div.champion-header-info");
-		//ar.remove(0);
-			
-			
-		//}
-		
-		//System.out.println(docTwo);
-		//System.out.println("Test" + Uname + "ASDF");
-		
-
-		
-		
+		Document ugg = Jsoup.connect("https://u.gg/lol/champions/"+ ar.get(0)+"/build?rank=challenger")
+				.timeout(6000).get();
+		System.out.println(ar.get(0));
+		Elements uggwinrate = ugg.select("div.win-rate.shinggo-tier");
+		Elements uggperswinrate = uggwinrate.select("div.value");
+				//System.out.println(uggwinrate.text());
+				Elements uggvsrate = ugg.select("div.overall-rank");
+				Elements uggpervsswinrate = uggvsrate.select("div.value");
+						//System.out.println(uggwinrate.text());
+					Elements uggpickrate = ugg.select("div.pick-rate");
+					Elements uggperpickswinrate = uggpickrate.select("div.value");
+							//System.out.println(uggwinrate.text());
+						Elements uggbankrate = ugg.select("div.ban-rate");
+					
 		//FILEWRITING 
-		FileWriter myWriter = new FileWriter(savefile + ".txt");
+		//FileWriter myWriter = new FileWriter(savefile + ".txt");
 		FileWriter htmlWriter = new FileWriter(savefile + ".html");
-		FileWriter csvWriter = new FileWriter(savefile + ".csv");
+		//FileWriter csvWriter = new FileWriter(savefile + ".csv");
 		
 		//TESTING
-		try {
-		      File myObj = new File(savefile);
-		      if (myObj.createNewFile()) {
-		    	
-		    	myWriter.write(profpic.toString() + wratio.toString());
-		  		myWriter.close();
-		  		
-		  		
-		  		htmlWriter.write(name.toString() + profpic.toString() + wratio.toString());
-		  		htmlWriter.close();
-		  		
-		  		
-		  		csvWriter.write(profpic.toString() + wratio.toString());
-		  		csvWriter.close();
-		        System.out.println("File created: " + myObj.getName());		        
-		      
-		      } else {
-		    	myWriter.write(profpic.toString() + wratio.toString());
-		  		myWriter.close();
-		  		
-		  		
-		  		htmlWriter.write(name.toString() + profpic.toString() + wratio.toString());
-		  		htmlWriter.close();
-		  		
-		  		
-		  		csvWriter.write(profpic.toString() + wratio.toString());
-		  		csvWriter.close();
-		        System.out.println("Filename already exists. Content updated.");
-
-		      }
-		    } catch (IOException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
+		htmlWriter.write("<h1> Summoner name: </h1>" + name.toString() + "</br>");
+		htmlWriter.write("SoloQue Division: " + mtier + "</br>" + "FlexQue Division: " + "</br>" + subt.text() + "</br>");
+		htmlWriter.write("<h2> Your top champions: </h2>" + "</br>");
+		for(int i=0; i<ar.size(); i++) {
+			htmlWriter.write(ar.get(i) + " - " + wr.get(i) + " - " + ant.get(i) + " - "  + "</br>");
+			
+		}	
+		htmlWriter.write("<h2> Global stats on your main champion: </h2>" + "</br>");
+		htmlWriter.write("<h3>" + ar.get(0) + "</h3>");
+		htmlWriter.write(uggwinrate.text() + "</br> " + uggvsrate.text() + "</br> " + uggpickrate + "</br>" + uggbankrate);
+		
+		
+		htmlWriter.close();  		
+		}
 
 	
-	}
 
-
+	
 	
 }
 
